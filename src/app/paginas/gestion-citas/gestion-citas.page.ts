de5ade9f4d18c75bd8c8fa816bdb3a60c45a6c32
elizabeth <elizabeth.pincheira.caro@gmail.com>
2025-01-04 21:39:19 -0300

    se agregan las pages

---
+++ src/app/paginas/gestion-citas/gestion-citas.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-gestion-citas',
+  templateUrl: './gestion-citas.page.html',
+  styleUrls: ['./gestion-citas.page.scss'],
+  standalone: true,
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+})
+export class GestionCitasPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
