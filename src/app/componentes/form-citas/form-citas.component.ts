bd441ea5702c2709a9d3ec25e4b0173b0e397282
elizabeth <elizabeth.pincheira.caro@gmail.com>
2025-01-05 16:09:47 -0300

    se agregan validaciones

--- src/app/componentes/form-citas/form-citas.component.ts
+++ src/app/componentes/form-citas/form-citas.component.ts
@@ -1,12 +1,15 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { IonItem, IonButton, IonInput, IonCard, IonCardHeader, IonCardTitle  } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { addCircleOutline } from 'ionicons/icons';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-form-citas',
   templateUrl: './form-citas.component.html',
   styleUrls: ['./form-citas.component.scss'],
   standalone: true,
-  imports: [FormsModule]
+  imports: [FormsModule, IonItem, IonButton, IonInput, IonCard, IonCardHeader, IonCardTitle, CommonModule]
 })
 export class FormCitasComponent  implements OnInit {
   fraseStr:string = '';
@@ -14,15 +17,46 @@ export class FormCitasComponent  implements OnInit {
 
   @Output() onCreate = new EventEmitter<{frase:string;autor:string}>()
 
-  constructor() { }
+  constructor() { 
+    addIcons({addCircleOutline});
+  }
+
+
+  mensajeFrase: boolean = false
+  mensajeAutor: boolean = false
 
   ngOnInit() {}
 
-  onClick(){
+onClick(){
     this.onCreate.emit({frase:this.fraseStr, autor:this.autorStr})
     this.fraseStr = '';
     this.autorStr = '';
+  }
 
+  async agregar() {
+    // Reinicia los mensajes de error antes de validar
+    this.mensajeFrase = false;
+    this.mensajeAutor = false;
+  
+    if (this.fraseStr.length < 5) {
+      console.log("El campo Frase debe tener al menos 5 caracteres");
+      this.mensajeFrase = true;
+    }
+  
+    if (this.autorStr.length < 2) {
+      console.log("El campo Autor debe tener al menos 2 caracteres");
+      this.mensajeAutor = true;
+    }
+  
+    // Solo emite el evento si no hay errores
+    if (!this.mensajeFrase && !this.mensajeAutor) {
+      this.onCreate.emit({ frase: this.fraseStr, autor: this.autorStr });
+      this.fraseStr = '';
+      this.autorStr = '';
+    }
   }
 
 }
+
+
+
--- src/app/paginas/configuracion/configuracion.page.ts
+++ src/app/paginas/configuracion/configuracion.page.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar} from '@ionic/angular/standalone';
+
 
 @Component({
   selector: 'app-configuracion',
   templateUrl: './configuracion.page.html',
   styleUrls: ['./configuracion.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonBackButton]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class ConfiguracionPage implements OnInit {
 
